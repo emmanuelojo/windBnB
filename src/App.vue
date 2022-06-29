b6aa8dbc4971927d8d61b57960b53d2a51b6cb37
emmanuelojo <emmanuelojo1234@gmail.com>
2022-06-29 12:13:54 +0100

    Mobile responsive

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,86 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Navbar from './components/Navbar.vue'
-// import Home from './views/Home.vue'
-import store from './store';
+import { onMounted, ref, watch } from "vue";
+import Navbar from "./components/Navbar.vue";
+import store from "./store";
 
 onMounted(() => {
-  data.value
-})
+  data.value;
+});
 
-const searchTerm = ref('')
+const searchTerm = ref("");
 
-const data = ref(store.getters.locations)
+const data = ref(store.getters.locations);
 
-const search = () => {
-
-}
-
+const search = () => {};
 </script>
 
 <template>
-  <div class="lg:w-[85%] lg:mx-auto">    
-    <Navbar @searchParams="search"/>
+  <div class="lg:w-[85%] lg:mx-auto">
+    <Navbar @searchParams="search" />
 
     <div class="mt-10 flex flex-col gap-8 mb-20 px-4 md:px-6">
-     <div class="flex items-end justify-between">
-       <p class="font-bold text-2xl">All</p>
+      <div class="flex items-end justify-between">
+        <p class="font-bold text-2xl">All</p>
 
         <p class="text-xs">12+ stays</p>
-     </div>
+      </div>
 
-      <div class="grid sm:grid-cols-n-2 lg:grid-cols-n-3 place-content-center gap-10 sm:gap-6 lg:gap-9">
-       <div v-for="(location, idx) in data" :key="idx" class=" w-full grid gap-3 cursor-pointer">
-          <img :src="location.photo" alt="Photo" class="h-52 w-full object-cover rounded-xl">
-         <!-- <div v-for="(location, idx) in data" :key="idx" class=" w-[350px] grid gap-3 cursor-pointer">
-          <img :src="location.photo" alt="Photo" class="h-52 w-[350px] object-cover rounded-xl"> -->
-
+      <div
+        class="grid sm:grid-cols-n-2 lg:grid-cols-n-3 place-content-center gap-10 sm:gap-6 lg:gap-9"
+      >
+        <div
+          v-for="(location, idx) in data"
+          :key="idx"
+          class="w-full grid gap-3 cursor-pointer"
+        >
+          <img
+            :src="location.photo"
+            alt="Photo"
+            class="h-52 w-full object-cover rounded-xl"
+          />
           <div class="flex items-center justify-between">
             <div class="flex items-center gap-2">
-              <div v-if="location.superHost" class="border border-black rounded-2xl w-24 h-7 flex justify-center items-center">
-                <p class="text-[13px] uppercase font-bold"> super host </p>
+              <div
+                v-if="location.superHost"
+                class="border border-black rounded-2xl w-24 h-7 flex justify-center items-center hover:bg-n-black hover:text-n-white"
+              >
+                <p class="text-[13px] uppercase font-bold">super host</p>
               </div>
 
               <div>
-                <p class="text-gray-400 text-sm"> {{ location.type }} </p>
+                <p class="text-gray-400 text-sm">{{ location.type }}</p>
               </div>
             </div>
 
             <div class="flex items-center gap-2">
               <span class="material-icons text-red-500"> star</span>
-              <p class=" font-normal"> {{ location.rating }}</p>
+              <p class="font-normal">{{ location.rating }}</p>
             </div>
           </div>
 
           <div>
-            <p class="font-semibold"> {{ location.title }} </p>
+            <p class="font-semibold">{{ location.title }}</p>
           </div>
         </div>
-
       </div>
-
     </div>
 
-    <div class="mt-60 mb-4 flex justify-center items-center text-xs text-gray-500 font-semibold attribution">
-      created by <a href="https://jekon-dev.netlify.app/" class="ml-1">Jekon</a> -  <a href="https://devchallenges.io/" target="_blank" class="ml-1"> devChallenges.io</a> 
+    <div
+      class="mt-60 mb-4 flex justify-center items-center text-xs text-gray-500 font-semibold attribution"
+    >
+      created by
+      <a href="https://jekon-dev.netlify.app/" class="ml-1">Jekon</a> -
+      <a href="https://devchallenges.io/" target="_blank" class="ml-1">
+        devChallenges.io</a
+      >
     </div>
   </div>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Mulish:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Mulish:wght@300;400&display=swap");
+
 #app {
-  font-family: 'Mulish';
+  font-family: "Mulish";
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,138 +1,261 @@
 <script setup lang="ts">
-import { ref,reactive, computed } from 'vue';
-import store from '../store';
+import { ref, reactive, computed } from "vue";
+import store from "../store";
 
-const showSearchOptions = ref(false)
+const showSearchOptions = ref(false);
 
-const searchTerm = ref('')
+const searchingLocation = ref(false);
 
-const numberOfAdults = ref(0)
+const addingGuests = ref(false);
 
-const numberOfChildren = ref(0)
+const searchTerm = ref("");
 
-const data = ref(store.getters.locations)
+const numberOfAdults = ref(0);
+
+const numberOfChildren = ref(0);
+
+const data = ref(store.getters.locations);
 
 const filteredLocations = computed(() => {
-    return data.value.filter(location => {
-        // if(location.country.toLowerCase().includes(searchTerm.value.toLowerCase()) || location.city.toLowerCase().includes(searchTerm.value.toLowerCase())) return location
-    return location.country.toLowerCase().includes(searchTerm.value.toLowerCase()) || location.city.toLowerCase().includes(searchTerm.value.toLowerCase())
-    })
-})
+  return data.value.filter((location) => {
+    // if(location.country.toLowerCase().includes(searchTerm.value.toLowerCase()) || location.city.toLowerCase().includes(searchTerm.value.toLowerCase())) return location
+    return (
+      location.country.toLowerCase().includes(searchTerm.value.toLowerCase()) ||
+      location.city.toLowerCase().includes(searchTerm.value.toLowerCase())
+    );
+  });
+});
 
-const totalGuests = computed(() => `${numberOfAdults.value + numberOfChildren.value} guests`)
+const totalGuests = computed(() => {
+  if (numberOfAdults.value + numberOfChildren.value < 1) {
+    return "Add guests";
+  } else {
+    return `${numberOfAdults.value + numberOfChildren.value} ${
+      numberOfAdults.value + numberOfChildren.value > 1 ? "guests" : "guest"
+    }`;
+  }
+});
+
+const searchLocation = () => {
+  searchingLocation.value = true;
+  addingGuests.value = false;
+};
+
+const addGuests = () => {
+  addingGuests.value = true;
+  searchingLocation.value = false;
+};
 
 const searchDetails = reactive({
-    location: searchTerm.value,
-    guests:{
-        numberOfAdults: numberOfAdults.value,
-        umberOfChildren: numberOfChildren.value
-    } 
-})
+  location: searchTerm.value,
+  guests: {
+    numberOfAdults: numberOfAdults.value,
+    umberOfChildren: numberOfChildren.value,
+  },
+});
 
 const increaseCount = (text: string) => {
-  text === 'a' ? numberOfAdults.value++ : numberOfChildren.value++
+  text === "a" ? numberOfAdults.value++ : numberOfChildren.value++;
 };
 
 const decreaseCount = (text: string) => {
-  if (numberOfAdults.value > 0 && text === 'a' )  numberOfAdults.value--
+  if (numberOfAdults.value > 0 && text === "a") numberOfAdults.value--;
 
-  if(numberOfChildren.value > 0 && text !== 'a') numberOfChildren.value--
+  if (numberOfChildren.value > 0 && text !== "a") numberOfChildren.value--;
 };
 
-const handleSubmit = () => {
+const closeModal = () => {
+    showSearchOptions.value = false
+    searchTerm.value = ''
+    numberOfAdults.value =  0
+    numberOfChildren.value = 0
 
 }
 
+const handleSubmit = () => {
+  showSearchOptions.value = false;
+};
 </script>
 
 <template>
-    <div class="relative">
-        <nav class="p-3 flex justify-between items-center">
-            <div>
-                <img src="../assets/logo.svg" alt="Logo">
-            </div>
+  <div class="relative">
+    <nav class="p-3 grid sm:flex sm:justify-between items-center">
+      <div>
+        <img src="../assets/logo.svg" alt="Logo" />
+      </div>
 
-            <div>
-                <div @click="showSearchOptions= true" class="h-10 flex items-center rounded-xl shadow cursor-pointer">
-                    <div class="px-3">
-                        <p class="text-xs">Lagos, Nigeria</p>
-                    </div>
-                    <div class="h-10 px-3 flex items-center justify-center border-l border-r border-gray-200">
-                        <p class="text-xs text-gray-300"> Add guests</p>
-                    </div>
-                    <div class="px-3 text-red-500 ">
-                        <span class="material-icons text-lg">search</span>
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-        <div v-if="showSearchOptions" class="h-fit px-5 py-10 absolute top-0 left-0 right-0 bg-white">
-            <form @submit.prevent="handleSubmit" class="grid grid-cols-n-3 rounded-xl shadow">
-                <div class="flex flex-col gap-10">
-                    <div class="flex flex-col pl-3 py-2 border border-black rounded-2xl">
-                        <label for="" class="text-[9px] font-bold uppercase">location</label>
-                        <input type="text" v-model="searchTerm" placeholder="Add location" class="placeholder:text-gray-400 placeholder:text-[13px] text-[13px] border-none outline-none">
-                    </div>
-
-                    <div v-if="searchTerm.length">
-                        <div v-for="(location, idx) in filteredLocations" :key="idx">
-                            <div class="flex items-center ">  <span class="material-icons "> room</span> <span>{{location.city + ', ' + location.country}} </span></div>
-                        </div>
-                    </div>
-                    <div v-if="searchTerm.length && !filteredLocations.length" class="-mt-5 pb-10 flex justify-center items-center">
-                        <p>No data found</p>
-                    </div>
-                </div>
-                <div class=" ">
-                    <div class="flex flex-col pl-3 py-2">
-                        <label for="" class="text-[9px] font-bold uppercase">guests</label>
-                        <input type="text" v-model="totalGuests" placeholder="Add guests" class="placeholder:text-gray-400 placeholder:text-[13px] text-[13px] border-none outline-none">
-                    </div>
-
-                    <div class="my-10 grid gap-10">
-                        <div class="flex flex-col gap-2">
-                            <div>
-                                <p class="text-sm font-semibold"> Adults</p>
-                                <p class="text-n-grey text-sm">Ages 13 and above</p>
-                            </div>
-
-                            <div class="text-n-grey flex items-center gap-6">
-                                <div class="w-6 h-6 flex justify-center items-center border border-n-black rounded-md cursor-pointer">
-                                 <i @click="decreaseCount('a')" class="fa fa-minus text-xs"></i>
-                                </div>
-                                <p class="text-n-black">{{ numberOfAdults}} </p>
-                                <div class="w-6 h-6 flex justify-center items-center border border-n-black rounded-md cursor-pointer">
-                                    <i @click="increaseCount('a')" class="fa fa-plus text-xs"></i>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div  class="flex flex-col gap-2">
-                            <div>
-                                <p class="text-sm font-semibold"> Children</p>
-                                <p class="text-n-grey text-sm">Ages 2 - 12</p>
-                            </div>
-
-                            <div  class="text-n-grey flex items-center gap-6">
-                                <div class="w-6 h-6 flex justify-center items-center border border-n-black rounded-md cursor-pointer">
-                                    <i @click="decreaseCount('c')" class="fa fa-minus text-xs"></i>
-                                </div>
-                                <p class="text-n-black">{{ numberOfChildren}} </p>
-                                <div class="w-6 h-6 flex justify-center items-center border border-n-black rounded-md cursor-pointer">
-                                    <i @click="increaseCount('c')" class="fa fa-plus text-xs"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="h-8 my-1 flex justify-center items-center  ">                    
-                    <div @click="handleSubmit" class="w-20 h-8 text-white text-xs flex justify-center items-center gap-[6px] bg-red-400 rounded-xl cursor-pointer hover:scale-105">
-                        <span class="material-icons text-sm">search</span>
-                        <p class="">Search</p>
-                    </div>
-                </div>
-            </form>
+      <div>
+        <div
+          @click="showSearchOptions = true"
+          class="mt-8 sm:mt-0 mx-auto w-[85%] sm:w-full h-10 flex items-center rounded-xl shadow cursor-pointer"
+        >
+          <div class="px-3 flex flex-grow items-center">
+            <p class="text-xs flex items-center">Lagos, Nigeria</p>
+          </div>
+          <div
+            class="h-10 px-3 flex flex-grow items-center justify-center border-l border-r border-gray-200"
+          >
+            <p class="text-xs text-gray-300">Add guests</p>
+          </div>
+          <div class="px-3 text-red-500">
+            <span class="material-icons text-lg">search</span>
+          </div>
         </div>
+      </div>
+    </nav>
+
+    <div
+      v-if="showSearchOptions"
+      class="h-fit sm:px-5 sm:pt-2 pb-5 md:py-10 absolute top-0 left-0 right-0 bg-white"
+    >
+      <div class="flex justify-between items-center p-2 sm:pb-5 md:hidden">
+        <p class="text-n-black font-semibold text-sm">Edit your search</p>
+        <div
+          @click="closeModal"
+          class="text-n-black cursor-pointer"
+        >
+          <i class="fa fa-times"></i>
+        </div>
+      </div>
+
+      <form @submit.prevent="handleSubmit" class="px-5 sm:px-0">
+        <div class="grid sm:grid-cols-n-3 rounded-xl shadow">
+          <div @click="searchLocation" class="flex flex-col gap-10">
+            <div
+              class="flex flex-col pl-3 py-2"
+              :class="
+                searchingLocation ? 'rounded-2xl border border-black' : ''
+              "
+            >
+              <label for="" class="text-[9px] font-bold uppercase"
+                >location</label
+              >
+              <input
+                type="text"
+                v-model="searchTerm"
+                placeholder="Add location"
+                class="placeholder:text-gray-400 placeholder:text-[13px] text-[13px] border-none outline-none"
+              />
+            </div>
+          </div>
+          <div @click="addGuests" class=" ">
+            <div
+              class="flex flex-col pl-3 py-2 border-l border-r"
+              :class="
+                addingGuests
+                  ? 'rounded-2xl border border-black'
+                  : 'border-n-white'
+              "
+            >
+              <label for="" class="text-[9px] font-bold uppercase"
+                >guests</label
+              >
+              <input
+                type="text"
+                v-model="totalGuests"
+                placeholder="Add guests"
+                class="placeholder:text-gray-400 placeholder:text-[13px] text-[13px] border-none outline-none"
+                :class="
+                  numberOfAdults + numberOfChildren < 1
+                    ? 'text-n-grey'
+                    : 'text-n-black'
+                "
+              />
+            </div>
+          </div>
+          <div class="my-1 hidden sm:flex justify-center items-center">
+            <div
+              @click="handleSubmit"
+              class="w-24 h-10 text-white text-xs flex justify-center items-center gap-[6px] bg-red-400 rounded-xl cursor-pointer hover:scale-105"
+            >
+              <span class="material-icons text-sm">search</span>
+              <p class="">Search</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="grid grid-cols-n-3">
+          <div
+            class="mt-8 pl-4"
+            :class="searchingLocation ? 'sm:visible' : 'invisible'"
+          >
+            <div v-if="searchTerm.length" class="grid gap-4">
+              <div v-for="(location, idx) in filteredLocations" :key="idx">
+                <div class="flex items-center text-n-dark-grey cursor-pointer">
+                  <span class="material-icons mr-2"> room</span>
+                  <span>{{ location.city + ", " + location.country }} </span>
+                </div>
+              </div>
+            </div>
+            <div
+              v-if="searchTerm.length && !filteredLocations.length"
+              class="flex justify-center items-center w-40 mx-auto"
+            >
+              <p class="">No data found</p>
+            </div>
+          </div>
+
+          <div v-if="addingGuests" class="my-10 ml-5 flex flex-col gap-10">
+            <div class="flex flex-col gap-2">
+              <div>
+                <p class="text-sm font-semibold">Adults</p>
+                <p class="text-n-grey text-sm">Ages 13 and above</p>
+              </div>
+
+              <div class="text-n-grey flex items-center gap-6">
+                <div
+                  class="w-6 h-6 flex justify-center items-center border border-n-black rounded-md cursor-pointer"
+                >
+                  <i
+                    @click="decreaseCount('a')"
+                    class="fa fa-minus text-xs"
+                  ></i>
+                </div>
+                <p class="text-n-black">{{ numberOfAdults }}</p>
+                <div
+                  class="w-6 h-6 flex justify-center items-center border border-n-black rounded-md cursor-pointer"
+                >
+                  <i @click="increaseCount('a')" class="fa fa-plus text-xs"></i>
+                </div>
+              </div>
+            </div>
+
+            <div class="flex flex-col gap-2">
+              <div>
+                <p class="text-sm font-semibold">Children</p>
+                <p class="text-n-grey text-sm">Ages 2 - 12</p>
+              </div>
+
+              <div class="text-n-grey flex items-center gap-6">
+                <div
+                  class="w-6 h-6 flex justify-center items-center border border-n-black rounded-md cursor-pointer"
+                >
+                  <i
+                    @click="decreaseCount('c')"
+                    class="fa fa-minus text-xs"
+                  ></i>
+                </div>
+                <p class="text-n-black">{{ numberOfChildren }}</p>
+                <div
+                  class="w-6 h-6 flex justify-center items-center border border-n-black rounded-md cursor-pointer"
+                >
+                  <i @click="increaseCount('c')" class="fa fa-plus text-xs"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="sm:hidden my-1 flex justify-center items-center">
+          <div
+            @click="handleSubmit"
+            class="w-24 h-10 text-white text-xs flex justify-center items-center gap-[6px] bg-red-400 rounded-xl cursor-pointer hover:scale-105"
+          >
+            <span class="material-icons text-sm">search</span>
+            <p class="">Search</p>
+          </div>
+        </div>
+      </form>
     </div>
+  </div>
 </template>
